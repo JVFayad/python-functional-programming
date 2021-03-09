6ad43a1f68a7f8901d55dfa8c1ec88edc25a2a72
JVFayad <joao.victor.fa2014@gmail.com>
2021-03-09 20:56:33 -0300

    Map function Examples

---
+++ map_function/map_ex.py
@@ -0,0 +1,32 @@
+import collections
+
+from pprint import pprint
+
+
+Scientist = collections.namedtuple(
+    'Scientist',
+    [
+        'name',
+        'field',
+        'born',
+        'nobel'
+    ]
+)
+
+scientists = (
+    Scientist(name='Ada Lovelace', field='math', born=1815, nobel=False),
+    Scientist(name='Emmy Noether', field='math', born=1882, nobel=False),
+    Scientist(name='Marie Curie ', field='math', born=1867, nobel=True),
+    Scientist(name='Tu Youyou', field='physics', born=1930, nobel=True),
+    Scientist(name='Ada Yonath', field='chemistry', born=1939, nobel=True),
+    Scientist(name='Vera Rubin', field='chemistry', born=1928, nobel=False),
+    Scientist(name='Sally Ride', field='physics', born=1951, nobel=False)
+)
+
+def transform(x):
+    return {'name': x.name, 'age': 2021 - x.born}
+
+# use map function to apply a function to all tuple itens
+results = tuple(map(transform, scientists))
+
+pprint(results)
