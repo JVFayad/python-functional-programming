d2a1b101b6cc0874fce324eb917973cb9dd38d56
JVFayad <joao.victor.fa2014@gmail.com>
2021-03-09 20:55:11 -0300

    Mutable Data Structures Examples

---
+++ mutable_data_structures/lists_and_dicts_ex.py
@@ -0,0 +1,8 @@
+scientists = [
+    {'name': 'Ada Lovelace', 'field': 'math', 'born': 1815, 'nobel': False},
+    {'name': 'Emmy Noether', 'field': 'math', 'born': 1882, 'nobel': False}
+]
+
+scientists[0]['name'] = 'any'
+
+print(scientists[0]['name'])
