9135b418d0862a8d21d886e64f24033e7d3fc62b
JVFayad <joao.victor.fa2014@gmail.com>
2021-03-09 21:00:06 -0300

    Parallel processing using Concurrent futures Examples

---
+++ parallel_processing/concurrent_futures_ex.py
@@ -0,0 +1,46 @@
+import collections
+import concurrent.futures
+import os
+import time
+
+from pprint import pprint
+
+
+Scientist = collections.namedtuple(
+    'Scientist',
+    [
+        'name',
+        'field',
+        'born',
+        'nobel'
+    ]
+)
+
+scientists = (
+    Scientist(name='Ada Lovelace', field='math', born=1815, nobel=False),
+    Scientist(name='Emmy Noether', field='math', born=1882, nobel=False),
+    Scientist(name='Marie Curie ', field='math', born=1867, nobel=True),
+    Scientist(name='Tu Youyou', field='physics', born=1930, nobel=True),
+    Scientist(name='Ada Yonath', field='chemistry', born=1939, nobel=True),
+    Scientist(name='Vera Rubin', field='chemistry', born=1928, nobel=False),
+    Scientist(name='Sally Ride', field='physics', born=1951, nobel=False)
+)
+
+def transform(x):
+    print(f'Process {os.getpid()} working record {x.name}')
+    time.sleep(3)
+    result = {'name': x.name, 'age': 2021 - x.born}
+    print(f'Process {os.getpid()} done working record {x.name}')
+    return result
+
+start_time = time.time()
+
+# use concurrent.futures ProcessPoolExecutor to use more CPU cores, or
+# ThreadPoolExecutor to use threads
+with concurrent.futures.ProcessPoolExecutor() as executor:
+    results = executor.map(transform, scientists)
+
+end_time = time.time()
+
+print(f'\nTime to complete: {end_time - start_time:.2f}')
+pprint(tuple(results))
